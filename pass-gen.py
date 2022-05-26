de651e7c3c723ffda6ee29787a5f50f16c344371
voyager-2021 <85347478+voyager-2021@users.noreply.github.com>
2022-05-26 11:51:11 +0300

    Create pass-gen.py

---
+++ pass-gen.py
@@ -0,0 +1,11 @@
+import random
+
+lower_case = "abcdefghijklmnopqrstuvwxyz"
+upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+nuber = "0123456789"
+symbols = "@#$%&*/\?!€;:()-_=+[]{}.,'"
+
+Use_For = lower_case + upper_case + nuber + symbols
+length_for_pass = 8
+
+password ="".join(random
